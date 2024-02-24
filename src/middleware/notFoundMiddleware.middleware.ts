4afc86b33cee9f0a65907d2c6ad81df6076c9de1
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2024-02-24 16:45:06 +0200

    hw8

---
+++ src/middleware/notFoundMiddleware.middleware.ts
@@ -0,0 +1,7 @@
+import {
+    Request,
+    Response,
+    NextFunction
+} from 'express';
+
+export const notFoundMiddleware = (req: Request, res: Response, next: NextFunction) => res.status(404).json({ error: 'Not Found' });
